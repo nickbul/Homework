ef08b33c62cf5760fa4afc41b9b29aad3d449342
nickbul <highpower@abv.bg>
2017-01-28 00:15:45 +0200

    Add files via upload

---
+++ Web_form/WebApplication1/WebForm1.aspx.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+
+namespace WebApplication1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        string Path = "C:/DataBase.txt";
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (SubmitValidation() == true)
+            {
+                this.ErrMsg1.Text = "";
+                this.ErrMsg1.Enabled = false;
+
+                string[] Vals = new string[4];
+
+                Vals[0] = this.Label1.Text + " " + this.UsrName.Text;
+                Vals[1] = this.Label2.Text + " " + this.Pass.Text;
+                Vals[2] = this.Label4.Text + " " + this.Age.Text;
+                Vals[3] = this.Label5.Text + " " + this.Email.Text;
+
+                using (StreamWriter writer = new StreamWriter(Path, true))
+                {
+                    for (int i = 0; i < Vals.Length; i++)
+                    {
+                        writer.Write(Vals[i] + " | ");
+                    }
+                    writer.WriteLine();
+                }
+                ClearBoxes();
+                ReadText();
+
+            }
+            
+        }
+
+        private void ReadText()
+        {
+            using (StreamReader reader = new StreamReader(Path))
+            {
+                var line = reader.ReadLine();
+                this.ListBox1.Items.Clear();
+
+                while (line != null && line != string.Empty)
+                {
+                    this.ListBox1.Items.Add(line);
+                    line = reader.ReadLine();
+                }
+            }
+        }
+
+        private void ClearBoxes()
+        {
+            this.UsrName.Text = "";
+            this.Age.Text = "";
+            this.Pass.Text = "";
+            this.Rpass.Text = "";
+            this.Email.Text = "";
+        }
+
+        private bool SubmitValidation()
+        {
+            int x = 0;
+            if (this.UsrName.Text != null && this.UsrName.Text != string.Empty)
+            {
+                x++;
+            }
+            else
+            {
+                this.ErrMsg1.Enabled = true;
+                this.ErrMsg1.Text = "No User name!";
+                this.ErrMsg1.ForeColor = System.Drawing.Color.Red;
+                return false;
+            }
+
+            if (this.Pass.Text != null && this.Pass.Text != string.Empty)
+            {
+                x++;
+            }
+            else
+            {
+                this.ErrMsg1.Enabled = true;
+                this.ErrMsg1.Text = "Please enter password!";
+                this.ErrMsg1.ForeColor = System.Drawing.Color.Red;
+                return false;
+            }
+
+            if (this.Rpass.Text != null && this.Rpass.Text != string.Empty)
+            {
+                x++;
+            }
+            else
+            {
+                this.ErrMsg1.Enabled = true;
+                this.ErrMsg1.Text = "Re-enter password!";
+                this.ErrMsg1.ForeColor = System.Drawing.Color.Red;
+                return false;
+            }
+
+            if (this.Pass.Text == this.Rpass.Text)
+            {
+                x++;
+            }
+            else
+            {
+                this.ErrMsg1.Enabled = true;
+                this.ErrMsg1.Text = "Passwords don't match!";
+                this.ErrMsg1.ForeColor = System.Drawing.Color.Red;
+                return false;
+            }
+
+            try
+            {
+                int age = int.Parse(this.Age.Text);
+                if (age > 8 && age < 100)
+                {
+                    x++;
+                }
+                else
+                {
+                    this.ErrMsg1.Enabled = true;
+                    this.ErrMsg1.Text = "Please enter your age!";
+                    this.ErrMsg1.ForeColor = System.Drawing.Color.Red;
+                    return false;
+                }
+            }
+            catch (Exception)
+            {
+
+                this.Age.Text = "";
+                this.ErrMsg1.Enabled = true;
+                this.ErrMsg1.Text = "Please enter your age using the scroll!";
+                this.ErrMsg1.ForeColor = System.Drawing.Color.Red;
+                return false;
+            }
+            
+            
+
+            if (this.Email.Text != null && this.Email.Text != string.Empty)
+            {
+                x++;
+            }
+            else
+            {
+                
+                this.ErrMsg1.Enabled = true;
+                this.ErrMsg1.Text = "Please enter your e-mail!";
+                this.ErrMsg1.ForeColor = System.Drawing.Color.Red;
+                return false;
+            }
+
+            if (x==6)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+    }
+}
---
+++ Win_Web_Forms/WindowsForms_App/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsForms_App
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            ReadText();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            if (CheckValid() == true)
+            {
+                string[] Vals = new string[3];
+
+                Vals[0] = label1.Text + " " + textBox1.Text;
+                Vals[1] = label2.Text + " " + textBox2.Text;
+                Vals[2] = label3.Text + " " + textBox3.Text;
+
+                using (StreamWriter writer = new StreamWriter("DataBase.txt",true))
+                {
+                    for (int i = 0; i < Vals.Length; i++)
+                    {
+                        writer.Write(Vals[i] + " | ");
+                    }
+                    writer.WriteLine();
+                }
+                ClearBoxes();
+                ReadText();
+
+                
+            }
+        }
+
+        private void ReadText()
+        {
+            using (StreamReader reader = new StreamReader("DataBase.txt"))
+            {
+                var line = reader.ReadLine();
+                listBox1.Items.Clear();
+
+                while (line != null && line != string.Empty)
+                {
+                    listBox1.Items.Add(line);
+                    line = reader.ReadLine();
+                }
+            }
+        }
+
+        private void ClearBoxes()
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+        }
+
+        public bool CheckValid()
+        {
+            int j = 0;
+
+            if (textBox1.Text != null && textBox1.Text != string.Empty)
+            {
+                j++;
+            }
+
+            if (textBox2.Text != null && textBox2.Text != string.Empty)
+            {
+                j++;
+            }
+
+            if (textBox3.Text != null && textBox3.Text != string.Empty)
+            {
+                if (textBox3.Text.Length > 5 || textBox3.Text.Contains("@") == true || textBox3.Text.Contains(".") == true)
+                {
+                    j++;
+                }
+            }
+
+            if (j==3)
+            {
+                return true;
+            }
+            else
+            {
+                return false;    
+            }
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ClearBoxes();
+        }
+
+
+    }
+}
